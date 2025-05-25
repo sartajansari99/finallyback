0f44f4d686703cdc3741f422faf4bf637207a291
sartaj ansari <sartaansari121sa@gmail.com>
2025-05-25 15:19:22 +0530

    updated

--- src/routes/subject.routes.js
+++ src/routes/subject.routes.js
@@ -4,13 +4,11 @@ import {
   getAllSubjects,
   updateSubject,
   deleteSubject,
-  myname
 } from "../controllers/subject.controllers.js";
 const router = Router();
 router.route("/createSubject").post(createSubject);
 router.route("/getAllSubjects").get(getAllSubjects);
-router.route("/updateSubject").post(updateSubject);
-router.route("/deleteSubject").post(deleteSubject);
-router.route("/deleteSubject").get(myname);
+router.route("/updateSubject:id").post(updateSubject);
+router.route("/deleteSubject:id").post(deleteSubject);
 
 export default router;
