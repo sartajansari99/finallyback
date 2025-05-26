a1a8273225ba831d2254fd387296e633177ca897
sartaj ansari <sartaansari121sa@gmail.com>
2025-05-26 13:07:53 +0530

    updated

--- src/routes/attendence.routes.js
+++ src/routes/attendence.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { markAttendance } from "../controllers/rfidLogic.controller.mjs";
+import { markAttendance } from "../controllers/rfidLogic.controller.js";
 const router = Router();
 router.route("/rfidAttendence").post(markAttendance);
 
