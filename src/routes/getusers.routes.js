fb650dd35da9675c81b2caf9030aafed61f38657
sartaj ansari <sartaansari121sa@gmail.com>
2025-06-03 09:19:07 +0530

    fsd

--- src/routes/getusers.routes.js
+++ src/routes/getusers.routes.js
@@ -1,10 +1,12 @@
 import express from "express";
 const router = express.Router();
-import {getAllUsers,getUserById} from "../controllers/getusers.controller.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import {
+  getAllUsers,
+  getUserById,
+} from "../controllers/getusers.controller.js";
 
-router.get('/getalluser', getAllUsers);
-router.get('/getuserbyid', getUserById);
-
-
+router.get("/getalluser", verifyJWT, getAllUsers);
+router.get("/getuserbyid", verifyJWT, getUserById);
 
 export default router;
