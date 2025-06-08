a03f536e0ae49c603e71ba14791aa5b74cd5e9db
sartaj ansari <sartaansari121sa@gmail.com>
2025-06-08 09:19:10 +0530

    Fa

--- src/routes/getusers.routes.js
+++ src/routes/getusers.routes.js
@@ -1,11 +1,12 @@
 import express from "express";
 const router = express.Router();
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 import {
   getAllUsers,
   getUserById,
 } from "../controllers/getusers.controller.js";
 
-router.get("/getalluser", getAllUsers);
+router.get("/getalluser", verifyJWT, getAllUsers);
 router.get("/getuserbyid", getUserById);
 
 export default router;
