f189e495217e89881f70d3a5adea6150f38898c6
sartaj ansari <sartaansari121sa@gmail.com>
2025-06-18 08:35:17 +0530

    Fsaf

--- src/app.js
+++ src/app.js
@@ -4,12 +4,11 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: "https://frontend-eta-ivory-62.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true
+}));
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
